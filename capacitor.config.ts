f7d3a23e45d9582895244ef50e821922e3b7147a
Peezzied <gamerpeenoise@gmail.com>
2025-03-21 22:25:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fare Kiosk UI',
+  webDir: 'dist'
+};
+
+export default config;
